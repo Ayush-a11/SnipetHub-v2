30c1b1548e2d0191d3e40b36524c0f098773a952
ayush-a11 <aayush.dimri1@gmail.com>
2024-10-03 23:10:58 +0530

    Firebase setup

---
+++ src/Firebase/firebaseConfig.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDh5YY-Sb5uR1n-A6zyshhMsLLTRBPGYg",
+  authDomain: "snippethub-v2-fe4fb.firebaseapp.com",
+  projectId: "snippethub-v2-fe4fb",
+  storageBucket: "snippethub-v2-fe4fb.appspot.com",
+  messagingSenderId: "366722868642",
+  appId: "1:366722868642:web:3577201a6ea4f3ed163efb"
+};
+
+// Initialize Firebase
+
+
+export default firebaseConfig;
